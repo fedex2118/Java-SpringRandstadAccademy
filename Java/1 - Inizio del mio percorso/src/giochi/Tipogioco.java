0a92570f0611b70f66dbc6a582ab10ed056095ec
Federico Ziegler <105019871+fedex2118@users.noreply.github.com>
2023-10-31 11:00:52 +0100

    retry

---
+++ Java/1 - Inizio del mio percorso/src/giochi/Tipogioco.java
@@ -0,0 +1,19 @@
+package giochi;
+
+public class Tipogioco {
+	String tipo;
+	
+	public Tipogioco(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+}
